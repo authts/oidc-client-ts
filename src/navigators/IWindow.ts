f77455d9fd79fd4755122025df90fe86ce3c5b7f
pamapa <pamapa@users.noreply.github.com>
2021-07-21 12:25:23 +0200

    fix: type

--- src/navigators/IWindow.ts
+++ src/navigators/IWindow.ts
@@ -2,6 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 export interface IWindow {
-    navigate(params: any): Promise<unknown>;
+    navigate(params: any): Promise<any>;
     close(): void;
 }
