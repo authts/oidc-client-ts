c8f70f02e2ce04997ccdb437ede0150f80a0441b
pamapa <pamapa@users.noreply.github.com>
2021-08-26 12:15:45 +0200

    feat: #4 reduce typescript warning/error ignores

--- src/navigators/IFrameWindow.ts
+++ src/navigators/IFrameWindow.ts
@@ -40,12 +40,14 @@ export class IFrameWindow implements IWindow {
         if (!params || !params.url) {
             this._error("No url provided");
         }
+        else if (!this._frame) {
+            this._error("No _frame, already closed");
+        }
         else {
             const timeout = params.silentRequestTimeout || DefaultTimeout;
             Log.debug("IFrameWindow.navigate: Using timeout of:", timeout);
             this._timer = window.setTimeout(this._timeout.bind(this), timeout);
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            this._frame!.src = params.url;
+            this._frame.src = params.url;
         }
 
         return this._promise;
@@ -69,18 +71,20 @@ export class IFrameWindow implements IWindow {
     }
 
     protected _cleanup() {
-        if (this._frame) {
-            Log.debug("IFrameWindow: cleanup");
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            window.removeEventListener("message", this._boundMessageEvent!, false);
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            window.clearTimeout(this._timer!);
-            window.document.body.removeChild(this._frame);
-
-            this._timer = null;
-            this._frame = null;
-            this._boundMessageEvent = null;
+        Log.debug("IFrameWindow: cleanup");
+        if (this._timer) {
+            window.clearTimeout(this._timer);
         }
+        if (this._boundMessageEvent) {
+            window.removeEventListener("message", this._boundMessageEvent, false);
+        }
+        if (this._frame) {
+            window.document.body.removeChild(this._frame);
+        }
+
+        this._timer = null;
+        this._boundMessageEvent = null;
+        this._frame = null;
     }
 
     protected _timeout() {
