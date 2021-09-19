cf83872bf96d43e21f6b4d2a77a28e64c487da2e
rvdkooy <ronald@sharpsolutions.nl>
2021-09-19 19:46:27 +0200

    restored COOP & COEP headers

--- .proxyrc.js
+++ .proxyrc.js
@@ -8,11 +8,8 @@ module.exports = function (app) {
     );
 
     app.use((req, res, next) => {
-        // window.opener && popup.closed will be
         res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
         res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
-        // res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-        // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 		next();
 	});
 
