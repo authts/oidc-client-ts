898ae46412b1fe2b0c051515b23ece91ca3630b8
Kyle Herock <kherock@gmail.com>
2021-09-23 11:31:13 -0400

    build: bundle version string from package.json

--- src/Version.ts
+++ src/Version.ts
@@ -1,4 +1,7 @@
+// @ts-expect-error avoid enabling resolveJsonModule to keep build process simple
+import { version } from "../package.json";
+
 /**
  * @public
  */
-export const Version = "2.0.0-alpha2";
+export const Version: string = version;
