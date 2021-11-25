f15ba9ed84cb9d5f309a0a3fc966038c05799b42
pamapa <pamapa@users.noreply.github.com>
2021-11-25 09:50:23 +0100

    feat: add unit-test for TokenClient

---
+++ src/TokenClient.test.ts
@@ -0,0 +1,244 @@
+import { Log } from "./utils";
+import { TokenClient } from "./TokenClient";
+import { MetadataService } from "./MetadataService";
+import { OidcClientSettings, OidcClientSettingsStore } from "./OidcClientSettings";
+
+describe("TokenClient", () => {
+    let settings: OidcClientSettings;
+    let metadataService: MetadataService;
+    let subject: TokenClient;
+
+    beforeEach(() => {
+        Log.logger = console;
+        Log.level = Log.NONE;
+
+        settings = {
+            authority: "authority",
+            client_id: "client_id",
+            redirect_uri: "redirect_uri"
+        };
+        const settingsStore = new OidcClientSettingsStore(settings);
+        metadataService = new MetadataService(settingsStore);
+
+        subject = new TokenClient(settingsStore, metadataService);
+    });
+
+    describe("exchangeCode", () => {
+        it("should have client_id", async () => {
+            // arrange
+            settings.client_id = "";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeCode({ code: "code", code_verifier: "code_verifier" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have redirect_uri", async () => {
+            // arrange
+            settings.redirect_uri = "";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeCode({ code: "code", code_verifier: "code_verifier" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have code", async () => {
+            // act
+            await expect(subject.exchangeCode({ code: "", code_verifier: "code_verifier" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have code_verifier", async () => {
+            // act
+            await expect(subject.exchangeCode({ code: "code", code_verifier: "" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have client_secret when using client_secret_basic", async () => {
+            // arrange
+            delete settings.client_secret;
+            settings.client_authentication = "client_secret_basic";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeCode({ code: "code", code_verifier: "code_verifier" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should calculate basic auth when using client_secret_basic", async () => {
+            // arrange
+            settings.client_authentication = "client_secret_basic";
+            settings.client_secret = "client_secret";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+            const getTokenEndpointMock = jest.spyOn(subject["_metadataService"], "getTokenEndpoint")
+                .mockResolvedValue("http://sts/token_endpoint");
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.exchangeCode({ code: "code", code_verifier: "code_verifier" });
+
+            // assert
+            expect(getTokenEndpointMock).toBeCalledWith(false);
+            expect(postFormMock).toBeCalledWith(
+                "http://sts/token_endpoint",
+                expect.anything(),
+                expect.anything()
+            );
+        });
+
+        it("should call postForm", async () => {
+            // arrange
+            const getTokenEndpointMock = jest.spyOn(subject["_metadataService"], "getTokenEndpoint")
+                .mockResolvedValue("http://sts/token_endpoint");
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.exchangeCode({ code: "code", code_verifier: "code_verifier" });
+
+            // assert
+            expect(getTokenEndpointMock).toBeCalledWith(false);
+            expect(postFormMock).toBeCalledWith(
+                "http://sts/token_endpoint",
+                expect.anything(),
+                undefined
+            );
+        });
+    });
+
+    describe("exchangeRefreshToken", () => {
+        it("should have client_id", async () => {
+            // arrange
+            settings.client_id = "";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeRefreshToken({ refresh_token: "refresh_token" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have redirect_uri", async () => {
+            // arrange
+            settings.redirect_uri = "";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeRefreshToken({ refresh_token: "refresh_token" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have refresh_token", async () => {
+            // act
+            await expect(subject.exchangeRefreshToken({ refresh_token: "" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should have client_secret when using client_secret_basic", async () => {
+            // arrange
+            delete settings.client_secret;
+            settings.client_authentication = "client_secret_basic";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            // act
+            await expect(subject.exchangeRefreshToken({ refresh_token: "refresh_token" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should calculate basic auth when using client_secret_basic", async () => {
+            // arrange
+            settings.client_authentication = "client_secret_basic";
+            settings.client_secret = "client_secret";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+            const getTokenEndpointMock = jest.spyOn(subject["_metadataService"], "getTokenEndpoint")
+                .mockResolvedValue("http://sts/token_endpoint");
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.exchangeRefreshToken({ refresh_token: "refresh_token" });
+
+            // assert
+            expect(getTokenEndpointMock).toBeCalledWith(false);
+            expect(postFormMock).toBeCalledWith(
+                "http://sts/token_endpoint",
+                expect.anything(),
+                expect.anything()
+            );
+        });
+
+        it("should call postForm", async () => {
+            // arrange
+            const getTokenEndpointMock = jest.spyOn(subject["_metadataService"], "getTokenEndpoint")
+                .mockResolvedValue("http://sts/token_endpoint");
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.exchangeRefreshToken({ refresh_token: "refresh_token" });
+
+            // assert
+            expect(getTokenEndpointMock).toBeCalledWith(false);
+            expect(postFormMock).toBeCalledWith(
+                "http://sts/token_endpoint",
+                expect.anything(),
+                undefined
+            );
+        });
+    });
+
+    describe("revoke", () => {
+        it("should have token", async () => {
+            // act
+            await expect(subject.revoke({ token: "", token_type_hint: "access_token" }))
+                // assert
+                .rejects.toThrow(Error);
+        });
+
+        it("should gracefully handle optional", async () => {
+            // arrange
+            jest.spyOn(subject["_metadataService"], "getRevocationEndpoint")
+                .mockResolvedValue(undefined);
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.revoke({ token: "token", token_type_hint: "access_token" });
+
+            // assert
+            expect(postFormMock).not.toBeCalled();
+        });
+
+        it("should call postForm", async () => {
+            // arrange
+            settings.client_secret = "client_secret";
+            subject = new TokenClient(new OidcClientSettingsStore(settings), metadataService);
+
+            const getTokenEndpointMock = jest.spyOn(subject["_metadataService"], "getRevocationEndpoint")
+                .mockResolvedValue("http://sts/revoke_endpoint");
+            const postFormMock = jest.spyOn(subject["_jsonService"], "postForm")
+                .mockResolvedValue({});
+
+            // act
+            await subject.revoke({ token: "token", token_type_hint: "access_token"  });
+
+            // assert
+            expect(getTokenEndpointMock).toBeCalledWith(false);
+            expect(postFormMock).toBeCalledWith(
+                "http://sts/revoke_endpoint",
+                expect.anything()
+            );
+        });
+    });
+});
