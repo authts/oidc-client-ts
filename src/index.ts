32c3e1895e4cc6fdf673c8b3b3dd31cb3b42ad0e
Badisi <badisi84@gmail.com>
2021-12-20 01:18:50 +0100

    fix(#278): expose the internal logger

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 export type { IFrameWindowParams, PopupWindowParams, RedirectParams } from "./navigators";
-export { Log } from "./utils";
+export { Log, Logger } from "./utils";
 export type { ILogger, PopupWindowFeatures } from "./utils";
 
 export { AccessTokenEvents } from "./AccessTokenEvents";
